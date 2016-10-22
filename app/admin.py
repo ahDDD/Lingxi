19372edc31913340325fd6e6d46960e3b70f7d4e
d <d664216@gmail.com>
2016-10-23 00:33:17 +0800

    注册admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from app.models import UserProfile, Question, Answer
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Question)
+admin.site.register(Answer)
