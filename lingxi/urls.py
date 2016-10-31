d70c86013d5a254e86a8e4ed1ba1cc07e9af4f2c
d <d664216@gmail.com>
2016-10-31 22:49:34 +0800

    新增用户头像

--- lingxi/urls.py
+++ lingxi/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from app.views import base
 
@@ -22,3 +24,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^base/', base)
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
