44d8d2ecb0e4d2553e32b1ec7dd196207229f80b
d <d664216@gmail.com>
2016-11-03 10:18:01 +0800

    修改了拉取新信息

--- app/views.py
+++ app/views.py
@@ -6,7 +6,10 @@ from app.models import Question, Answer, UserProfile
 def base(request):
     context = {}
     questions = Question.objects.all().order_by('-create_time')
-    context['questions'] = questions
+    user_profile = UserProfile.objects.get(belong_to=request.user)
+    context['questions'] = questions[:5]
+    context['new_question'] = user_profile.user_question.all().order_by('-create_time').first()
+    context['new_answer'] = user_profile.user_answer.all().order_by().first()
     user_profile = UserProfile.objects.all()
     context['user_profile'] = user_profile
     return render(request, 'base.html', context)
